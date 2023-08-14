eb7b15e21aeac9c78acb50e18c8412a591d39b1a
Zayb333 <75285716+Zayb333@users.noreply.github.com>
2023-08-13 22:13:47 -0400

    zaylinburris-developer

---
+++ script.js
@@ -0,0 +1,40 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        card.addEventListener("click", function () {
+            this.classList.toggle("expanded");
+        });
+    });
+});
+
+const cards = document.querySelectorAll('.card', '.about-card');
+window.addEventListener('scroll', checkBoxes);
+checkBoxes();
+
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault;
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth";
+        })
+    })
+}); 
+
+
+function checkboxes() {
+    const trigger = window.innerHeight / 5 * 4;
+    cards.forEach((card, idx) => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if (cardTop < trigger) {
+            card.classList.add('show');
+        }
+        else {
+            card.classList.remove('show');
+        }
+    });
+}
